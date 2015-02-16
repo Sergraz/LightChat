ebf14fe549415d5ee1a331fb7ce5fa256b1c6201
chervontsev <schervontsev@lokata.ru>
2015-02-16 11:04:05 +0300

    added redirect from root

---
+++ src/RootFilter.java
@@ -0,0 +1,32 @@
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class RootFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        String uri = request.getRequestURI();
+        String contextPath = request.getContextPath();
+        if (uri.replace("/", "").equals(contextPath.replace("/", ""))) {
+            response.sendRedirect("chat.jsp");
+        } else {
+            chain.doFilter(req, res);
+        }
+
+
+    }
+}
